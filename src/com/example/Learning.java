6c34519d2fa7c12126e16279f5e8fd440e8d6119
Maria Eduarda Duarte <dudammduarte@yahoo.com.br>
2023-05-29 16:31:30 -0300

    add main function

--- src/com/example/Learning.java
+++ src/com/example/Learning.java
@@ -1,4 +1,15 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Learning {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        int number = Integer.parseInt(line);
+
+        System.out.println(number + 1);
+    }
 }
