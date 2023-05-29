de1cc0690215f3f4a0b7710224167f864d9a1f30
Thais Kaneshiro <thais.kaneshiro@nubank.com.br>
2023-05-29 17:00:02 -0300

    create new function and call this at main

--- src/com/example/Learning.java
+++ src/com/example/Learning.java
@@ -6,10 +6,16 @@ import java.io.InputStreamReader;
 
 public class Learning {
     public static void main(String[] args) throws IOException {
+        System.out.println("Digite um numero:");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line = reader.readLine();
         int number = Integer.parseInt(line);
+        // String resultado = Integer.toString(incremento(number)); 
+        // System.out.println("Seu resultado é: "+resultado);
+        System.out.println(String.format("Seu resultado é: %x", incremento(number)));
+    }
 
-        System.out.println(number + 1);
+    private static int incremento(int number){
+        return number + 1;
     }
 }
