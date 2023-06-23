99043841409979cbbb8ccd4717fdf887d3582090
jademcosta <jade.costa@nubank.com.br>
2023-06-23 09:03:57 -0300

    Rename to MyIntegerArray

--- src/com/example/inheritance/exercise/MyIntegerArray.java
+++ src/com/example/inheritance/exercise/MyIntegerArray.java
@@ -3,17 +3,17 @@ package com.example.inheritance.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyArray {
+public class MyIntegerArray {
   private ArrayList<Integer> array;
 
-  public MyArray(operador?, int[] ints) {
+  public MyIntegerArray(operador?, int[] ints) {
     array = new ArrayList<Integer>();
     for (int i : ints) {
       array.add(i);
     }
   }
 
-  public MyArray(operador?, List<Integer> ints) {
+  public MyIntegerArray(operador?, List<Integer> ints) {
     array = new ArrayList<Integer>();
     array.addAll(ints);
   }
