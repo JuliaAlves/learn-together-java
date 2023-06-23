53f3b18ca83f23c5f665a4b16531cf59633d60a3
jademcosta <jade.costa@nubank.com.br>
2023-06-23 09:01:52 -0300

    Add array operator exercise

---
+++ src/com/example/inheritance/exercise/MyArray.java
@@ -0,0 +1,28 @@
+package com.example.inheritance.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyArray {
+  private ArrayList<Integer> array;
+
+  public MyArray(operador?, int[] ints) {
+    array = new ArrayList<Integer>();
+    for (int i : ints) {
+      array.add(i);
+    }
+  }
+
+  public MyArray(operador?, List<Integer> ints) {
+    array = new ArrayList<Integer>();
+    array.addAll(ints);
+  }
+
+  public ArrayList<Integer> ordered() {
+
+  }
+
+  public ArrayList<Integer> reverse() {
+
+  }
+}
